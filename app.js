da8efa2bd57c6aea0bef1a42d9ea4c734d62b5cb
Will Lewis <willy.lewis@gmail.com>
2020-03-29 15:46:33 +0100

    Migration from jade (deprecated) to pug

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express'),
 	Player = require('./poker_modules/player');
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // create "middleware"
 
