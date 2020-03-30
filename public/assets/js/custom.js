cbbbf2398731f0e17929c6a2e6b57c939248adde
Will Lewis <willy.lewis@gmail.com>
2020-03-30 17:13:13 +0100

    Design Changes

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,18 +1,3 @@
 $( document ).ready(function() {
-    
-	
-	//Login
-	//$("#loginModal").modal('show');
-	$("#loginModal").modal();
-	console.log("Custom.JS - loaded"); 
-	
-	$('#loginRegisterPopup').on( "click", function() {
-		//$('#loginModal').modal('show');
-		console.log("loginRegisterPopup - triggered");
-		alert('nice');
-	});
-	
 
-	
 });
-
